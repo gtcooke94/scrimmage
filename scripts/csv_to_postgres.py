4540b21aefdab278fefe3b43fb55e1b4f0587cab
Gregory Cooke <gtcooke94@gmail.com>
2018-06-28 13:07:03 -0400

    Use glob to get all csv files, name the tables based on the csv file name

--- scripts/csv_to_postgres.py
+++ scripts/csv_to_postgres.py
@@ -1,9 +1,10 @@
 """Copies csv files after a scrimmage run to a postgres database."""
 import argparse
 import os.path
+import glob
+import pdb
 import psycopg2
 from psycopg2 import sql
-#  import pdb
 #  from psycopg2 import sql
 
 # We need a log directory and datatypes fed to us
@@ -32,6 +33,7 @@ cur = conn.cursor()
 # Assume we pass through tables, columns, datatypes that we want
 # Build the table
 #  log_dir = '~/.scrimmage/logs/2018-06-28_09-41-11/'
+pdb.set_trace()
 timestamp = os.path.basename(os.path.dirname(log_dir))
 timestamp = timestamp.replace('-', '_')
 schema = 's' + timestamp
@@ -40,8 +42,6 @@ conn.commit()
 
 # Assume we have a list of tables that we want to make? Or can glob for csvs in
 # the log directory
-table = 'summary'
-full_table = "{}.{}".format(schema, table)
 #  data_types = [
 #  "INT PRIMARY KEY", "DOUBLE PRECISION",
 #  "DOUBLE PRECISION", "DOUBLE PRECISION", "DOUBLE PRECISION",
@@ -51,24 +51,31 @@ full_table = "{}.{}".format(schema, table)
 # for (col, data_type) in zip(columns, data_types):
 
 # Actually copy all of the data
-with open('/home/gcooke3/.scrimmage/logs/2018-06-27_14-23-09/summary.csv',
-          'r') as f:
-    # Notice that we don't need the `csv` module.
-    # next(f)  # Skip the header row.
-    cols = f.readline()
-    cols = cols.splitlines()[0]
-    cols = cols.split(',')
-    query_string = 'CREATE TABLE {} '.format(full_table)
-    to_join = ''.join([col + ' ' + data_type + ', ' for col, data_type in
-                       zip(cols, data_types)])
-    to_join = to_join[:-2]
-    to_join = "({});".format(to_join)
-    query_string = query_string + to_join
-    query = sql.SQL(query_string)
-    cur.execute(query)
-    #  cur = conn.cursor()
-    #  pdb.set_trace()
+files = glob.glob(log_dir + "*.csv")
+#  pdb.set_trace()
+for fi in files:
+    with open(fi, 'r') as f:
+        pdb.set_trace()
+        # Notice that we don't need the `csv` module.
+        # next(f)  # Skip the header row.
+        table = os.path.basename(os.path.splitext(fi)[0])
+        full_table = "{}.{}".format(schema, table)
+        cols = f.readline()
+        cols = cols.splitlines()[0]
+        cols = cols.split(',')
+        # could get datatypes with
+        # ["DOUBLE PRECISION" for i in range(0, len(cols)]
+        query_string = 'CREATE TABLE {} '.format(full_table)
+        to_join = ''.join([col + ' ' + data_type + ', ' for col, data_type in
+                           zip(cols, data_types)])
+        to_join = to_join[:-2]
+        to_join = "({});".format(to_join)
+        query_string = query_string + to_join
+        query = sql.SQL(query_string)
+        cur.execute(query)
+        #  cur = conn.cursor()
+        #  pdb.set_trace()
 
-    cur.copy_from(f, "{}".format(full_table), sep=',')
-    conn.commit()
-    #  f.close()
+        cur.copy_from(f, "{}".format(full_table), sep=',')
+        conn.commit()
+conn.close()
