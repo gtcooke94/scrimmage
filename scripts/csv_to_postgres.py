650ff60c8362eccd52e11c8a7db7abb090575541
Gregory Cooke <gtcooke94@gmail.com>
2018-06-28 13:35:40 -0400

    Changed to with _ as _: syntax

--- scripts/csv_to_postgres.py
+++ scripts/csv_to_postgres.py
@@ -21,56 +21,56 @@ data_types = args.data_types
 #  print(args.log_dir)
 
 # Connect to existing database
-conn = psycopg2.connect("dbname=scrimmage user=scrimmage password=scrimmage")
+connect_string = "dbname=scrimmage user=scrimmage password=scrimmage"
+with psycopg2.connect(connect_string) as conn:
 
-# Open cursor to perform operations
-cur = conn.cursor()
+    # Open cursor to perform operations
+    with conn.cursor() as cur:
+        cur = conn.cursor()
+        # Assume we have the log directory put through
 
-# Assume we have the log directory put through
+        # Assume we pass through tables, columns, datatypes that we want
+        # Build the table
+        #  log_dir = '~/.scrimmage/logs/2018-06-28_09-41-11/'
+        timestamp = os.path.basename(os.path.dirname(log_dir))
+        timestamp = timestamp.replace('-', '_')
+        schema = 's' + timestamp
+        cur.execute('CREATE SCHEMA IF NOT EXISTS ' + schema + ";")
+        # conn.commit() gets called automatically when using with syntax
+        # conn.commit()
 
-# Assume we pass through tables, columns, datatypes that we want
-# Build the table
-#  log_dir = '~/.scrimmage/logs/2018-06-28_09-41-11/'
-timestamp = os.path.basename(os.path.dirname(log_dir))
-timestamp = timestamp.replace('-', '_')
-schema = 's' + timestamp
-cur.execute('CREATE SCHEMA IF NOT EXISTS ' + schema + ";")
-conn.commit()
+    # Assume we have a list of tables that we want to make? Or can glob for
+    # csvs in the log directory
+    #  data_types = [
+    #  "INT PRIMARY KEY", "DOUBLE PRECISION",
+    #  "DOUBLE PRECISION", "DOUBLE PRECISION", "DOUBLE PRECISION",
+    #  "DOUBLE PRECISION", "DOUBLE PRECISION", "DOUBLE PRECISION"]
 
-# Assume we have a list of tables that we want to make? Or can glob for csvs in
-# the log directory
-#  data_types = [
-#  "INT PRIMARY KEY", "DOUBLE PRECISION",
-#  "DOUBLE PRECISION", "DOUBLE PRECISION", "DOUBLE PRECISION",
-#  "DOUBLE PRECISION", "DOUBLE PRECISION", "DOUBLE PRECISION"]
+    #  query = 'CREATE TABLE ' + schema + "." + table_name + "( "
+    # for (col, data_type) in zip(columns, data_types):
 
-#  query = 'CREATE TABLE ' + schema + "." + table_name + "( "
-# for (col, data_type) in zip(columns, data_types):
-
-# Actually copy all of the data
-files = glob.glob(log_dir + "*.csv")
-for fi in files:
-    with open(fi, 'r') as f:
-        # Notice that we don't need the `csv` module.
-        # next(f)  # Skip the header row.
-        table = os.path.basename(os.path.splitext(fi)[0])
-        full_table = "{}.{}".format(schema, table)
-        cols = f.readline()
-        cols = cols.splitlines()[0]
-        cols = cols.split(',')
-        # could get datatypes with
-        # ["DOUBLE PRECISION" for i in range(0, len(cols)]
-        query_string = sql.SQL('CREATE TABLE {} ').format(sql.SQL(full_table))
-        to_join = ''.join([col + ' ' + data_type + ', ' for col, data_type in
-                           zip(cols, data_types)])
-        to_join = to_join[:-2]
-        to_join_sql = sql.SQL("({});").format(sql.SQL(to_join))
-        query = query_string + to_join_sql
-        #  query = sql.SQL(query_string)
-        cur.execute(query)
-        #  cur = conn.cursor()
-        pdb.set_trace()
-
-        cur.copy_from(f, "{}".format(full_table), sep=',')
-        conn.commit()
+    # Actually copy all of the data
+    files = glob.glob(log_dir + "*.csv")
+    for fi in files:
+        with open(fi, 'r') as f:
+            # Notice that we don't need the `csv` module.
+            # next(f)  # Skip the header row.
+            table = os.path.basename(os.path.splitext(fi)[0])
+            full_table = "{}.{}".format(schema, table)
+            cols = f.readline()
+            cols = cols.splitlines()[0]
+            cols = cols.split(',')
+            # could get datatypes with
+            # ["DOUBLE PRECISION" for i in range(0, len(cols)]
+            query_string = sql.SQL('CREATE TABLE {} ').format(sql.SQL(full_table))
+            to_join = ''.join([col + ' ' + data_type + ', ' for col, data_type in
+                               zip(cols, data_types)])
+            to_join = to_join[:-2]
+            to_join_sql = sql.SQL("({});").format(sql.SQL(to_join))
+            query = query_string + to_join_sql
+            #  query = sql.SQL(query_string)
+            with conn.cursor() as cur:
+                cur.execute(query)
+                cur.copy_from(f, "{}".format(full_table), sep=',')
+                # conn.commit()
 conn.close()
